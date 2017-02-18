c7a2285eae0d4ad36cf4f1425e373705a16649b5
Kfir Stri <kfirstar@gmail.com>
2017-02-18 17:05:54 +0200

    refactored

--- weather.go
+++ weather.go
@@ -1,61 +1,63 @@
 package main
 
 import (
-    "fmt"
-	"os"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/url"
-	"encoding/json"
-	"io/ioutil"
+	"os"
 )
 
 const urlPath = "http://api.openweathermap.org/data/2.5/weather?units=metric&q=%s&appid=%s"
 const appID = "8f704952bd05684b4a6c36b1ccdc417e"
 
 type weatherResponse struct {
-    queryCity string
-    Name string
-    Weather []map[string]interface{}
-    Main map[string]float32
+	queryCity string
+	Name      string
+	Weather   []map[string]interface{}
+	Main      map[string]float32
 }
 
-func (wr *weatherResponse) loadCurrentWeather() error {
-    // Getting the data from the openweathermap API
-    resp, err := http.Get(fmt.Sprintf(urlPath, url.QueryEscape(wr.queryCity), appID))
+func getData(city string) (*http.Response, error) {
+    return http.Get(fmt.Sprintf(urlPath, url.QueryEscape(city), appID))
+}
 
-    if err != nil {
-        return err
-    }
+func (wr *weatherResponse) loadCurrentWeather() (err error) {
+    var response *http.Response
 
-    // Read the json response and unmarshal it to the current weather object
-    outputAsBytes, err := ioutil.ReadAll(resp.Body)
-    err = json.Unmarshal(outputAsBytes, wr)
+	// Getting the data from the openweathermap API
+    if response, err = getData(wr.queryCity); err != nil {
+		return err
+	}
 
-    if err != nil {
-        return err
-    }
+	// Read the json response and unmarshal it to the current weather object
+	outputAsBytes, err := ioutil.ReadAll(response.Body)
+	if err = json.Unmarshal(outputAsBytes, wr); err != nil {
+		return err
+	}
 
-    return nil
+	return nil
 }
 
 func main() {
-    var args = os.Args
-    var cityWeather weatherResponse
+	var args = os.Args
+	var cityWeather weatherResponse
 
-    if len(args) == 1 {
-        fmt.Println("Missing city argument")
-        return
-    }
+	if len(args) == 1 {
+		fmt.Println("Missing city argument")
+		return
+	}
 
-    // Get the city from the arguments list
-    cityWeather.queryCity = args[1]
+	// Get the city from the arguments list
+	cityWeather.queryCity = args[1]
 
-    err := cityWeather.loadCurrentWeather()
+	err := cityWeather.loadCurrentWeather()
 
-    if err != nil {
-        fmt.Println("there was an error", err)
-    }
+	if err != nil {
+		fmt.Println("there was an error", err)
+	}
 
-    fmt.Printf("=== Weather in %s ===\n", cityWeather.Name)
-    fmt.Printf("%s with tempature of %.2f celsius", cityWeather.Weather[0]["description"], cityWeather.Main["temp"])
+	fmt.Printf("=== Weather in %s ===\n", cityWeather.Name)
+	fmt.Printf("%s with tempature of %.2f celsius", cityWeather.Weather[0]["description"], cityWeather.Main["temp"])
 }
